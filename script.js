8d2078c319c922b77f45702ce14ee4b2f542de51
didi <eljamiyenaa@gmai.com>
2025-01-12 00:43:20 +0100

    ajoutee des effect avec jQuery

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 $(document).ready(function () {
     $(".scrollBtn").click(function () {
-        var targetId = $(this).data('target');  // Fetch the target ID
+        var targetId = $(this).data('target');
         $('html, body').animate({
-            scrollTop: $("#" + targetId).offset().top
-        }, 1000); // Smooth scroll to the section
+            scrollTop: $("#" + targetId).offset().top - 250
+        }, 1000);
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.scrollBtn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            buttons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('.light').hover(
+        function () {
+
+            $(this).css({
+                'border': '2px dashed transparent',
+                'box-shadow': '0 0 222px rgb(17, 0, 255), 0 0 100px rgb(0, 4, 255)',
+                'transform': 'translateY(55px) rotate(1deg)',
+                
+
+            });
+        },
+        function () {
+            $(this).css({
+                'border': '0px solid #ddd',
+                'box-shadow': '0 0 100px rgba(21, 0, 255, 0.59), 0 0 100px rgba(0, 4, 255, 0.54)',
+                'transform': 'rotate(0deg)'
+            });
+        }
+    );
+ });
+
